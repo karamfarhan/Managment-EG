a0e2473b38c4c89982fc65f64552717828fbad83
karamfarhan <www.karam777krm@gmail.com>
2023-01-03 12:51:25 +0300

    DN

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -225,7 +225,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "zahabiacompany@gmail.com"
-EMAIL_HOST_PASSWORD = "gppqpmvdtyroavia"
+EMAIL_HOST_PASSWORD = ""
 
 JAZZMIN_SETTINGS = {"show_ui_builder": True}
 
