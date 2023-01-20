27a0346e1dd82e7ad5260c80ccda65dce776bcd0
karamfarhan <www.karam777krm@gmail.com>
2023-01-20 20:22:36 +0300

    updates

--- backend/apps/employee/untils.py
+++ backend/apps/employee/untils.py
@@ -1,14 +1,14 @@
 def get_certificate_image_filepath(self, filename):
-    return f"employee_images/{self.name}/certificate/{self.certificate_image}.png"
+    return f"employee_images/{self.pk}/certificate/{self.certificate_image}.png"
 
 
 def get_experience_image_filepath(self, filename):
-    return f"employee_images/{self.name}/experience/{self.experience_image}.png"
+    return f"employee_images/{self.pk}/experience/{self.experience_image}.png"
 
 
 def get_identity_image_filepath(self, filename):
-    return f"employee_images/{self.name}/identity/{self.identity_image}.png"
+    return f"employee_images/{self.pk}/identity/{self.identity_image}.png"
 
 
 def get_criminal_record_image_filepath(self, filename):
-    return f"employee_images/{self.name}/criminal_record/{self.criminal_record_image}.png"
+    return f"employee_images/{self.pk}/criminal_record/{self.criminal_record_image}.png"
