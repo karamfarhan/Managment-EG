66e9f01422acea287964f87d37173fd665372c63
karamfarhan <www.karam777krm@gmail.com>
2023-01-20 22:41:39 +0300

    latest update with Veryacademy

--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -1,4 +1,7 @@
+import os
+
 from .base import *
+from .base import BASE_DIR
 
 DEBUG = False
 
@@ -9,7 +12,17 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
 ]
-
+DATABASES = {"default": {}}
+if os.environ.get("DEVELOPER") == "KARAM":
+    DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql_psycopg2"
+    DATABASES["default"]["NAME"] = os.environ.get("DB_NAME")
+    DATABASES["default"]["USER"] = os.environ.get("DB_USER")
+    DATABASES["default"]["PASSWORD"] = os.environ.get("DB_PASSWORD")
+    DATABASES["default"]["HOST"] = "pgdb"
+    DATABASES["default"]["PORT"] = 5432
+if os.environ.get("DEVELOPER") == "ALAA":
+    DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
+    DATABASES["default"]["NAME"] = BASE_DIR / "db.sqlite3"
 # ### PRODUCTION SETTING -- WITH AWS SERVIER
 # # - make an account in aws
 # # - make bucket in s3
